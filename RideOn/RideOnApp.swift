2ae1580f99103b930da6a78de60f79486e64392c
prabucki <wojtekprabucki@icloud.com>
2025-11-01 23:34:42 +0100

    Initial Commit

---
+++ RideOn/RideOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideOnApp.swift
+//  RideOn
+//
+//  Created by Wojciech Prabucki on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RideOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
